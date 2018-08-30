46df6d9aa874e9761eec014b6bf6246e78d19cd6
timmyreilly <timreilly@live.com>
2018-08-30 13:39:15 -0700

    MWHAHAHAHAHAHAHA I got it to work!

--- GetRating.cs
+++ GetRating.cs
@@ -25,10 +25,10 @@ namespace OrganicsThirdTry
             var limit = 100;
             var limitQueryParameter = req.Query["limit"];
 
-            var id = req.Query["id"]; 
-            log.Info(id); 
+            var id = req.Query["id"];
+            log.Info(id);
 
-            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 }; 
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
 
             // IQueryable<Rating> ratingQuery = documentClient.CreateDocumentQuery<Rating>(
             //     UriFactory.CreateDocumentCollectionUri("ChallengeTwo", "Ratings"), queryOptions).Where(i => i.id == id); 
@@ -44,14 +44,17 @@ namespace OrganicsThirdTry
             IDocumentQuery<Rating> rdog = documentClient.CreateDocumentQuery<Rating>(collectionUri).Where(rt => rt.id == id).AsDocumentQuery();
             // Assert("Expected only 1 rating", rdog.ToList().Count == 1); 
 
+            var guid = new System.Guid("9337a7bc-176c-4971-bf74-41537bb78ba9");
 
+            var rating = documentClient.CreateDocumentQuery<Rating>(collectionUri).Where(b => b.id == guid ).AsEnumerable().FirstOrDefault(); 
+            log.Info(rating.ToString()); 
 
             IDocumentQuery<Rating> query = documentClient.CreateDocumentQuery<Rating>(collectionUri)
                 .Take(limit)
                 .AsDocumentQuery();
 
             // return new OkObjectResult(await query.ExecuteNextAsync());
-            return new OkObjectResult("HERE YOU GO: " + await rdog.ExecuteNextAsync()); 
+            return new OkObjectResult(rating.ToString());
         }
 
         //     [FunctionName("CosmosDBSample")]
@@ -87,5 +90,5 @@ namespace OrganicsThirdTry
         //         }
         //     }
     }
-    
+
 }
--- Models/Rating.cs
+++ Models/Rating.cs
@@ -1,18 +1,24 @@
-using System; 
+using System;
+using Newtonsoft.Json; 
 
-namespace OrganicsThirdTry {
+namespace OrganicsThirdTry
+{
     public class Rating
-        {
-            public Guid id { get; set; }
-            public Guid userId { get; set; }
-            public Guid productId { get; set; }
-            public DateTime timestamp { get; set; }
-            public string locationName { get; set; }
-            public int rating { get; set; }
-            public string userNotes { get; set; }
-            // public int magicNumber { get; set; }
+    {
+        public Guid id { get; set; }
+        public Guid userId { get; set; }
+        public Guid productId { get; set; }
+        public DateTime timestamp { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+        // public int magicNumber { get; set; }
 
-            // public double sentimentScore { get; set; }
-            
+        // public double sentimentScore { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
         }
+
+    }
 }
