10805cacb4734696817503da7283b128f1be566c
timmyreilly <timreilly@live.com>
2018-08-29 17:03:46 -0700

    this is freaking rediciulatasdfasdf

--- GetRating.cs
+++ GetRating.cs
@@ -7,26 +7,60 @@ using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OrganicsThirdTry
 {
     public static class GetRating
     {
-        [FunctionName("GetRating")]
-        public static HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ratings/{id}")]HttpRequestMessage req,
-            [CosmosDB(
-                databaseName: "Challenge2",
+        // [FunctionName("GetRating")]
+        // public static HttpResponseMessage Run(
+        //     [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ratings/{id}")]HttpRequestMessage req,
+        //     [CosmosDB(
+        //         databaseName: "ChallengeTwo",
+        //         collectionName: "Ratings",
+        //         ConnectionStringSetting = "CosmosConnectionString",
+        //         Id = "{id}")]
+        //         //SqlQuery = "select * from Ratings r where r.id = {id}")]
+        //         OrganicsThirdTry.Rating document, TraceWriter log)
+        // {
+        //     if (document == null)
+        //     {
+        //         log.Info("Not found");
+        //         log.Info(document.ToString());
+        //         return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+        //     }
+        //     else
+        //     {
+        //         return req.CreateResponse(System.Net.HttpStatusCode.OK, document);
+        //     }
+        // }
+
+    //     [FunctionName("CosmosDBSample")]
+    //     public static HttpResponseMessage Run(
+    // [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "foo/{id}")] HttpRequestMessage req,
+    // [CosmosDB(databaseName: "ChallengeTwo",
+    //             collectionName: "Ratings",
+    //             ConnectionStringSetting = "CosmosConnectionString", Id = "{id}")] object document)
+    //     {
+    //         if (document == null)
+    //         {
+    //             return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+    //         }
+    //         else
+    //         {
+    //             return req.CreateResponse(System.Net.HttpStatusCode.OK, document);
+    //         }
+    //     }
+
+        [FunctionName("COSMSOSOSOSOSOSOS")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "foo/{id}")] HttpRequestMessage req,
+    [CosmosDB(databaseName: "ChallengeTwo",
                 collectionName: "Ratings",
-                ConnectionStringSetting = "CosmosConnectionString",
-                Id = "{id}")]
-                //SqlQuery = "select * from Ratings r where r.id = {id}")]
-                OrganicsThirdTry.Rating document, TraceWriter log)
+                ConnectionStringSetting = "CosmosConnectionString", Id = "{id}")] object document, TraceWriter log)
         {
             if (document == null)
             {
-                log.Info("Not found"); 
-                log.Info(document.ToString()); 
                 return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
             }
             else
