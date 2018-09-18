6b1b52bb6d8eddc717e60aa7c9a3a17cf67c25a0
timmyreilly <timreilly@live.com>
2018-09-17 17:06:29 -0700

    not sure where we're at

--- EventGridTrigger.cs
+++ EventGridTrigger.cs
@@ -8,6 +8,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Newtonsoft.Json.Linq;
 // using Microsoft.Azure.EventGrid.Models;
 
 
@@ -16,19 +17,9 @@ namespace Company.Function
     public static class EventGridTrigger
     {
         [FunctionName("EventGridTrigger")]
-        public static IActionResult Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public static void Run([EventGridTrigger]JObject eventGridEvent, TraceWriter log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            var name = eventGridEvent.Data; 
-
-            // var requestBody = new StreamReader(eventGridEvent).ReadToEnd();
-            dynamic data = name; 
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            log.Info(eventGridEvent.ToString(Formatting.Indented));
         }
     }
 }
